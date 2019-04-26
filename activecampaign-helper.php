592ac7d41243c69164ef107c0f2af818a1bb719f
cyberwombat <yashua@urbanlotus.com>
2019-04-26 11:22:36 -0700

    Update internal version number

--- activecampaign-helper.php
+++ activecampaign-helper.php
@@ -22,7 +22,7 @@ if (!defined('ABSPATH')) {
 /**
  * Required minimums and constants
  */
-define('AC_HELPER_VERSION', '0.0.4');
+define('AC_HELPER_VERSION', '0.0.9');
 define('AC_HELPER_MIN_PHP_VER', '5.6.0');
 define('AC_HELPER_MIN_WC_VER', '3.0.0');
 define('AC_HELPER_MAIN_FILE', __FILE__);
